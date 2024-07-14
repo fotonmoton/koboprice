64c26ab368dbcfeb9955817bdc28c1e3a6c3dadc
Gregory Tertyshny <neogen570@gmail.com>
2024-07-14 22:36:08 +0300

    partially working solution

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,12 @@ import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 import globals from "globals";
 
 export default [
-  {    
-    ignores: ["dist"]  
-  },
   {
-    ...js.configs.recommended,
-    ...eslintPluginPrettierRecommended,
+    ignores: ["dist"],
+  },
+  eslintPluginPrettierRecommended,
+  js.configs.recommended,
+  {
     rules: {
       "no-unused-vars": "warn",
       "no-undef": "warn",

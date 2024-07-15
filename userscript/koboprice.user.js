fec251aea9490f5a8ac3c82a6eb5420579e4a687
Gregory Tertyshny <neogen570@gmail.com>
2024-07-15 20:52:41 +0300

    icons

---
+++ userscript/koboprice.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name Kobo Price
+// @description finds lowest book price on kobo.com
+// @icon https://github.com/fotonmoton/koboprice/blob/master/icon.png?raw=true
+// @namespace https://github.com/fotonmoton
+// @run-at document-end
+// @noframes
+// @match https://www.kobo.com/*/*/ebook/*
+// @match https://www.kobo.com/*/*/audiobook/*
+// @require ../dist/koboprice.js
+// ==/UserScript==

94b688843cc5c6e36da4c575f7f4752c8ea43cf1
Gregory Tertyshny <neogen570@gmail.com>
2024-07-17 14:21:14 +0300

    build userscript from sources

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import globals from "globals";
 
 export default [
   {
-    ignores: ["dist"],
+    ignores: ["dist", "userscript"],
   },
   eslintPluginPrettierRecommended,
   js.configs.recommended,
